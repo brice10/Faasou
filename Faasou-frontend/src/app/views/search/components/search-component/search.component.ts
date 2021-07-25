8d956482c7640aeb33601d4b0ffecefa2d89fef7
BladeFinger <sanwouobrell@gmail.com>
2021-07-25 13:20:24 +0100

    fonction de recupération des images pour la classification dans les grid

--- Faasou-frontend/src/app/views/search/components/search-component/search.component.ts
+++ Faasou-frontend/src/app/views/search/components/search-component/search.component.ts
@@ -5,5 +5,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['search.component.css']
 })
 export class SearchComponent implements OnInit {
+
+  public allImages: any[] = [];
+
+  public firstColumnImages: any[] = [];
+
+  public secondColumnImages: any[] = [];
+
+  public thirdColumnImages: any[] = [];
+
   ngOnInit(): void {}
+
+  public initializeColumnImages() {
+
+    var allImagesSize = this.allImages.length;
+    const allImagesSizeDividedBy3 =Math.floor(allImagesSize/3);
+
+    var indiceTab=0;
+
+    do{
+      this.firstColumnImages[indiceTab] = this.allImages[indiceTab];
+      indiceTab++;
+    }while(( indiceTab % allImagesSizeDividedBy3) !=0 )
+
+     do{
+      this.secondColumnImages[indiceTab] = this.allImages[indiceTab];
+      indiceTab++;
+    }while((indiceTab%allImagesSizeDividedBy3) !=0 )
+
+    
+    do{
+      this.thirdColumnImages[indiceTab] = this.allImages[indiceTab];
+      indiceTab++;
+    }while(indiceTab < allImagesSize ) 
+  }
+
 }
